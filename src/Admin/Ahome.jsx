05f2ca5b934860ee31398860dc49d0dcb1504b74
Kushal <kushalobroy@gmail.com>
2023-09-08 20:51:31 +0530

    initial commit

--- src/Admin/Ahome.jsx
+++ src/Admin/Ahome.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function Ahome(){
     return (
         <div>
-            <h1 className="text-white">Welcome to the Admin Dashboard</h1><br/>
+            <h1 className="text-center">Welcome to the Admin Dashboard</h1><br/>
         </div>
     );
 }
--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Ahome from "./Admin/Ahome";
 import Ehome from "./Evaluator/Ehome";
-import Shome from "./Student/Shome"; 
+import Shome from "./Student/Shome";
 
 
 function App() {
-  
+
   return (
     <div className="App">
-     
+
 
       <Router>
-      <Routes>
-     
-        <Route path='/Admin' element={<Ahome/>}/>
-        <Route path='/Student' element={<Shome/>}/>
-        <Route path='/Evaluator' element={<Ehome/>}/>
-      </Routes>
-    </Router>
+        <Routes>
+
+          <Route path='/Admin' element={<Ahome />} />
+          <Route path='/Student' element={<Shome />} />
+          <Route path='/Evaluator' element={<Ehome />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
