631efa90c18561580e97db169c2ecd8978ce965b
Kushal <kushalobroy@gmail.com>
2023-09-10 23:25:27 +0530

    intial commit

--- src/Admin/Ahome.jsx
+++ src/Admin/Ahome.jsx
@@ -1,28 +1,9 @@
 import React from "react";
-import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
+import Sidebar from "./sidebar";
 function Ahome() {
   return (
     <>  
-       <div className="App">
-      <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
-        <Container>
-          <Navbar.Brand href="#">Admin</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#">Home</Nav.Link>
-              <Nav.Link href="#">Evaluator</Nav.Link>
-              <Nav.Link href="#">Stdudent</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
-      <Container fluid>
-        
-      </Container>
-    </div>
-
+       <Sidebar />
     </>
   );
 }
