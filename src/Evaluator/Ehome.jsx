fdc5ef9932c344e4dd5d9b3ed24648037922331e
Ajay Kumar Maurya <ajaykumarmaurya760@gmail.com>
2023-09-10 23:30:12 +0530

    initial commit

--- src/Evaluator/Ehome.jsx
+++ src/Evaluator/Ehome.jsx
@@ -3,17 +3,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function Ehome(){
     return (
         <div>
-            <nav class="navbar navbar-expand-sm bg-info">
-
-<div class="container-fluid">
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="#">Evaluator</a>
-    </li>
-  </ul>
-</div>
-
-</nav>
+           <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand text-primary mx-3 " href="">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item p-2">
+                        <a class="nav-link" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
         </div>
     );
 }
