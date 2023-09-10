7bfe8f2e82d4191841d8d40dffab37d3449a99a5
Kushal <kushalobroy@gmail.com>
2023-09-10 14:21:34 +0530

    initial commit

--- src/Evaluator/Ehome.jsx
+++ src/Evaluator/Ehome.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-function Ehome(){
-    return (
-        <div>
-            <nav class="navbar navbar-expand-sm bg-info">
+function Ehome() {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-sm bg-info">
 
-<div class="container-fluid">
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="#">Evaluator</a>
-    </li>
-  </ul>
-</div>
-
-</nav>
+        <div class="container-fluid">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Evaluator</a>
+              
+            </li>
+          </ul>
         </div>
-    );
+
+      </nav>
+    </div>
+  );
 }
 export default Ehome;
