ba055691043aa9a461fc96dfab73a90547e89f6c
Kushal <kushalobroy@gmail.com>
2023-09-10 17:27:40 +0530

    initial commit

--- src/Admin/Ahome.jsx
+++ src/Admin/Ahome.jsx
@@ -1,33 +1,30 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 function Ahome() {
   return (
-<div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Admin</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="">Home</a>
-            </li>
+    <>  
+       <div className="App">
+      <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+        <Container>
+          <Navbar.Brand href="#">Admin</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#">Home</Nav.Link>
+              <Nav.Link href="#">Evaluator</Nav.Link>
+              <Nav.Link href="#">Stdudent</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-          </ul>
-        </div>
-      </div>
-    </nav>
-   { /* Sidebar */}
-    <div>
-    
-    
-      
+      <Container fluid>
+        
+      </Container>
     </div>
-   { /* Side Bar */}
-      </div> 
 
+    </>
   );
 }
+
 export default Ahome;
