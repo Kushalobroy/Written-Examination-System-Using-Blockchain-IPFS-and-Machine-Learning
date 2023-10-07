1cdfc2bcc13268dcd9fed45b25dccc15f4aa2d35
unknown <skraj5872@gmail.com>
2023-10-07 11:51:33 +0530

    initial commit

---
+++ src/Student/TextEditor.jsx
@@ -0,0 +1,49 @@
+// src/components/TextEditor.js
+
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css"; // Import the styles for the editor
+
+function TextEditor() {
+  const [editorHtml, setEditorHtml] = useState("");
+
+  // Handle changes in the editor content
+  const handleEditorChange = (content) => {
+    setEditorHtml(content);
+  };
+
+  return (
+    <div>
+      <ReactQuill
+        onChange={handleEditorChange}
+        modules={TextEditor.modules}
+        formats={TextEditor.formats}
+        placeholder="Write your response here..."
+      />
+    </div>
+  );
+}
+
+// Define modules and formats for the editor
+TextEditor.modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+
+TextEditor.formats = [
+  "header",
+  "font",
+  "list",
+  "bold",
+  "italic",
+  "underline",
+  "link",
+  "image",
+];
+
+export default TextEditor;
