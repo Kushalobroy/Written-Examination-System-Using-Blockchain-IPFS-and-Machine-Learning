95801d53b9b204f5516f425777ed947de6fce19c
Kushal <kushalobroy@gmail.com>
2023-11-01 17:46:43 +0530

    initial commit

--- src/Admin/Aadd.jsx
+++ src/Admin/Aadd.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import '../App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "./sidebar"
@@ -8,6 +8,82 @@ function Aadd() {
     const Toggle = () => {
         setToggle(!toggle)
     }
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        contact_no: '',
+        username: '',
+        password: '',
+        photo: null,
+      });
+    
+      const handleFileChange = (event) => {
+        setFormData({
+          ...formData,
+          photo: event.target.files[0],
+        });
+      };
+    
+      const handleFormSubmit = async (event) => {
+        event.preventDefault();
+    
+        const data = new FormData();
+    
+        for (const key in formData) {
+          if (formData.hasOwnProperty(key)) {
+            if (key === 'photo') {
+              data.append('photo', formData[key]);
+            } else {
+              data.append(key, formData[key]);
+            }
+          }
+        }
+    
+        try {
+          const response = await fetch('http://localhost:3000/api/admin/add_admin', {
+            method: 'POST',
+            body: data,
+          });
+    
+          if (response.ok) {
+            // Handle success
+            alert('Admin registered successfully');
+          } else {
+            // Handle errors
+            alert('Error: Admin registration failed');
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+    //Display Admin List
+    const [admins, setAdmins] = useState([]);
+
+  useEffect(() => {
+    async function fetchAdmins() {
+      try {
+        const response = await fetch('http://localhost:3000/api/admin/get_admins',{
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setAdmins(data);
+          
+        } else {
+          console.error('Error: Failed to fetch admin data');
+        }
+      } catch (error) {
+        console.error('Network error: ' + error.message);
+      }
+    }
+
+    fetchAdmins();
+  }, []);
+
+    
     return (
         <div className="container-fluid bg-secondary min-vh-100">
             <div className="row">
@@ -21,29 +97,29 @@ function Aadd() {
                         <div className="row">
                             <div className="col-md-6">
                             <h4 className="text-white text-center fs-4">Add Admin</h4>
-                            <form action="">
-                                <div className="mb-3">
-                                    <input className="form-control" type="text" placeholder="Name" />
-                                </div>
-                                <div className="mb-3">
-                                    <input className="form-control" type="text" placeholder="email" />
-                                </div>
-                                <div className="mb-3">
-                                    <input className="form-control" type="text" placeholder="Contact No." />
-                                </div>
-                                <div className="mb-3">
-                                    <input className="form-control" type="text" placeholder="User id" />
-                                </div>
-                                <div className="mb-3">
-                                    <input className="form-control" type="text" placeholder="Password" />
-                                </div>
-                                <div className="mb-3">
-                                    <label className="form-label text-white float-start">Photo</label>
-                                <input className="form-control" type="file"/> 
-                                </div>
-                                <div className="mb-3">
-                                    <a className="btn btn-primary" href="">Add</a>
-                                </div>
+                            <form onSubmit={handleFormSubmit}>
+                            <div className="mb-3">
+                                <input class="form-control form-control-sm" type="text" placeholder="Name" onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
+                            </div>
+                            <div className="mb-3">
+                                <input class="form-control form-control-sm" type="text" placeholder="Email" onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
+                            </div>
+                            <div className="mb-3">
+                                <input class="form-control form-control-sm" type="text" placeholder="Contact Number" onChange={(e) => setFormData({ ...formData, contact_no: e.target.value })} />
+                            </div>
+                            <div className="mb-3">
+                                <input class="form-control form-control-sm" type="text" placeholder="Username" onChange={(e) => setFormData({ ...formData, username: e.target.value })} />
+                            </div>
+                            <div className="mb-3">
+                                <input class="form-control form-control-sm" type="password" placeholder="Password" onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="img" className="form-label text-white float-start">Photo</label>
+                                <input class="form-control form-control-sm" type="file" name="photo" id="img" onChange={handleFileChange} />
+                            </div>
+                            <div className="mb-3">
+                                <button type="submit" className="btn btn-primary btn-sm" href="">Add</button>
+                            </div>
                             </form>
                             </div>
                             <div className="col-md-6">
@@ -51,35 +127,25 @@ function Aadd() {
                                 <div className="table table-responsive">
                                     <table className="table table-striped">
                                         <thead>
-                                            <th>Name</th>
-                                            <th>Email</th>
-                                            <th>Contact No</th>
-                                            <th>Action</th>
+                                            <th scope="col">Profile</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Email</th>
+                                            <th scope="col">Contact No</th>
+                                            <th scope="col">Action</th>
                                         </thead>
                                         <tbody>
+                                        {admins.map((admin) => (
                                             <tr>
-                                                <td>Kushal</td>
-                                                <td>kushalobroy@gmail.com</td>
-                                                <td>7800286558</td>
-                                                <td><a className="btn btn-danger" href="">Delete</a></td>
+                                                
+                                                <td><img src={'/uploads/${admin.photo.filename}'} alt={`Image for ${admin.name}`} /></td>
+                                                <td>{admin.name}</td>
+                                                <td>{admin.email}</td>
+                                                <td>{admin.contact_no}</td>
+                                                <td><a className="btn btn-outline-danger btn-sm" href="">Delete</a></td>
                                             </tr>
+                                              ))}
                                             <tr>
-                                                <td>Sandeep Kumar</td>
-                                                <td>sk5873@gmail.com</td>
-                                                <td>9305525008</td>
-                                                <td><a className="btn btn-danger" href="">Delete</a></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Ajay</td>
-                                                <td>........@gmail.com</td>
-                                                <td>........</td>
-                                                <td><a className="btn btn-danger" href="">Delete</a></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Diksha</td>
-                                                <td>.........@gmail.com</td>
-                                                <td>...........</td>
-                                                <td><a className="btn btn-danger" href="">Delete</a></td>
+                                            
                                             </tr>
                                         </tbody>
                                     </table>
--- src/Home.jsx
+++ src/Home.jsx
@@ -7,26 +7,18 @@ function Home(){
             <div className="container-sm w-50 mb-3 mt-5 pt-5 pb-5 bg-light rounded shadow-lg p-3 mb">
             <h1 className="text-primary fs-1 fw-1 mt-3 mb-2">Login</h1>
             <form className="">
+               
                 <div className="mb-3">
-                <select className="form-control mt-3">
-                    <option value="-1">Select</option>
-                    <option value="Admin">Admin</option>
-                    <option value="Evaluator">Evaluator</option>
-                    <option value="Student">Student</option>
-                </select>
-                <label className="form-label float-start ms-1">Usertype</label>
+                <input type="text" className="form-control form-control-sm" placeholder="User id"></input>
+                <label className="form-label form-label-sm float-start ms-1">User Id</label>
                 </div>
                 <div className="mb-3">
-                <input type="text" className="form-control" placeholder="User id"></input>
-                <label className="form-label float-start ms-1">User Id</label>
+                <input type="password" className="form-control form-control-sm" placeholder="Password"></input>
+                <label className="form-label form-label-sm float-start ms-1">Password</label>
                 </div>
                 <div className="mb-3">
-                <input type="password" className="form-control" placeholder="Password"></input>
-                <label className="form-label float-start ms-1">Password</label>
-                </div>
-                <div className="mb-3">
-                    <a href="/Facerecognization" className="btn btn-primary" type="submit">Login</a>
-                    <button className="btn btn-danger ms-4" type="submit">Reset</button>
+                    <a href="/Facerecognization" className="btn btn-outline-primary btn-sm" type="submit">Login</a>
+                    <button className="btn btn-outline-danger btn-sm ms-4" type="submit">Reset</button>
                 </div>
             </form>
             </div>
